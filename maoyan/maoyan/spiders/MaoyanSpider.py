be6829e4b7c95b2af4dbe021e48a78b913640300
ZhangMJ <295692590@qq.com>
2018-06-29 18:06:51 +0800

    实现评分和票房解密

--- maoyan/maoyan/spiders/MaoyanSpider.py
+++ maoyan/maoyan/spiders/MaoyanSpider.py
@@ -3,6 +3,46 @@ from scrapy.spiders import Rule, CrawlSpider
 from scrapy.selector import Selector
 from scrapy.linkextractors import LinkExtractor
 from maoyan.items import MaoyanItem
+import urllib.request 
+import struct
+import zlib
+from fontTools.ttLib import TTFont
+import xml.dom.minidom as xmldom
+import os
+
+def getValue(node, attribute):
+	return node.attributes[attribute].value
+
+def getTTGlyphList(xml_path):
+	dataXmlfilepath = os.path.abspath(xml_path)
+	dataDomObj = xmldom.parse(dataXmlfilepath)
+	dataElementObj = dataDomObj.documentElement
+	dataTTGlyphList = dataElementObj.getElementsByTagName('TTGlyph')
+	return dataTTGlyphList
+
+def isEqual(ttglyph_a, ttglyph_b):
+	a_pt_list = ttglyph_a.getElementsByTagName('pt')
+	b_pt_list = ttglyph_b.getElementsByTagName('pt')
+	a_len = len(a_pt_list)
+	b_len = len(b_pt_list)
+	if a_len != b_len:
+		return False
+	for i in range(a_len):
+		if getValue(a_pt_list[i], 'x') != getValue(b_pt_list[i], 'x')  or getValue(a_pt_list[i], 'y') != getValue(b_pt_list[i], 'y') or getValue(a_pt_list[i], 'on') != getValue(b_pt_list[i], 'on'):
+			return False
+	return True
+
+def refresh(dict, ttGlyphList_a, ttGlyphList_data):
+	data_dict = {"uniE184":"4","uniE80B":"3","uniF22E":"8","uniE14C":"0",
+		"uniF5FB":"6","uniEE59":"5","uniEBD3":"1","uniED85":"7","uniECB8":"2","uniE96A":"9"}
+	data_keys = data_dict.keys()
+	for ttglyph_data in ttGlyphList_data:
+		if 	getValue(ttglyph_data,'name') in data_keys:
+			for ttglyph_a in ttGlyphList_a:
+				if isEqual(ttglyph_a, ttglyph_data):
+					dict[getValue(ttglyph_a,'name')] = data_dict[getValue(ttglyph_data,'name')]
+					break
+	return dict
 
 class MaoyanSpider(CrawlSpider):
 	name = 'maoyan'
@@ -37,6 +77,45 @@ class MaoyanSpider(CrawlSpider):
 		actor_src = sel.xpath('//li[@class="celebrity actor"]/a/img/@data-src').extract()
 		
 		introduction = sel.xpath('.//div[@class="mod-content"]/span/text()').extract_first()
+
+		#解码
+		#下载字体文件
+		font_url = sel.xpath('/html/head/style/text()').extract()[0]
+		font_url = 'http:'+font_url[font_url.rfind('url')+5:font_url.find('woff')+4]
+		print(font_url)
+		woff_path = 'tmp.woff'
+		f = urllib.request.urlopen(font_url)
+		data = f.read()
+		with open(woff_path, "wb") as code:
+			code.write(data)
+		#分析解码字典
+		font1 = TTFont('tmp.woff')
+		font1.saveXML('tmp.xml')
+		decode_dict = dict(enumerate(font1.getGlyphOrder()[2:]))
+		decode_dict=dict(zip(decode_dict.values(),decode_dict.keys()))	
+
+		dataTTGlyphList = getTTGlyphList("data.xml")
+		tmpTTGlyphList = getTTGlyphList("tmp.xml")
+		decode_dict = refresh(decode_dict,tmpTTGlyphList,dataTTGlyphList)
+		decode_dict['.'] = '.'
+		# print(decode_dict)
+
+		#解码
+		_lst_uincode = []
+		for item in movie_star.__repr__().split("\\u"):
+			_lst_uincode.append("uni" + item[:4].upper())
+			if item[4:]:
+				_lst_uincode.append(item[4:])
+		_lst_uincode = _lst_uincode[1:-1]
+		movie_star = "".join([str(decode_dict[i]) for i in _lst_uincode])
+		_lst_uincode = []
+		for item in movie_total_price.__repr__().split("\\u"):
+			_lst_uincode.append("uni" + item[:4].upper())
+			if item[4:]:
+				_lst_uincode.append(item[4:])
+		_lst_uincode = _lst_uincode[1:-1]
+		movie_total_price = "".join([str(decode_dict[i]) for i in _lst_uincode])
+
 
 		item = MaoyanItem()
 		item['movie_name'] = movie_name
